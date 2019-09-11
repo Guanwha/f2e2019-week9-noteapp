1eeddb4aaa87dfbbd0fe732d2af75a51c14c78e6
Eric Fei <guanwha@gmail.com>
2019-09-12 02:12:59 +0800

    Added useful settings in eslint
    1. allow multi-spaces before comments for reading
    2. allow else can be next-line
    3. allow function parameter indent
    4. allow unary operators (++, --) in for loop
    5. allow conditional / switchcase indent
    6. allow max-length in one-line

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+  extends: ['plugin:vue/essential', 'airbnb-base'],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      js: 'never',
+      vue: 'never'
+    }],
+    // disallow reassignment of function parameters
+    // disallow parameter object manipulation except for specific exclusions
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'state', // for vuex state
+        'acc', // for reduce accumulators
+        'e' // for e.returnvalue
+      ]
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      optionalDependencies: ['test/unit/index.js']
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // allow multi-spaces before comments for reading
+    'no-multi-spaces': ["error", { ignoreEOLComments: true }],
+    // allow else can be next-line
+    'brace-style': ["error", "stroustrup"],
+    // allow function parameter indent
+    'indent': ["error", 2, {"FunctionExpression": {"parameters": "first"}, "CallExpression": {"arguments": "first"}}],
+    // allow unary operators (++, --) in for loop
+    'no-plusplus': ["error", { "allowForLoopAfterthoughts": true }],
+    // allow conditional / switchcase indent
+    'indent': ["error", 2, { "ignoredNodes": ["ConditionalExpression"], "SwitchCase": 1 }],
+    // allow max-length in one-line
+    'max-len': ["error", { "code": 255 }],
+  }
+}
