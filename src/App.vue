09bf47b72d70a695443ad3134565a1d0a6dae91e
Eric Fei <guanwha@gmail.com>
2019-09-12 03:28:45 +0800

    Installed bootstrap 4.3.1
    1. modify App.vue for test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about" class="mr-auto">About</router-link>
+      </nav>
     </div>
     <router-view/>
   </div>
@@ -15,15 +18,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
