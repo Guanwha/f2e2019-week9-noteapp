b3bdedc0241e5384d70a7c14be206094e3363926
Eric Fei <guanwha@gmail.com>
2019-09-15 19:56:18 +0800

    Added pages router settings

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Test from '../pages/Test.vue';
+// import Test from '../pages/Test.vue';
+import Notes from '../pages/Notes.vue';
+import Note from '../pages/Note.vue';
+import EditNote from '../pages/EditNote.vue';
 
 Vue.use(Router);
 
@@ -8,10 +11,25 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'test',
+    //   component: Test,
+    // },
     {
       path: '/',
-      name: 'test',
-      component: Test,
+      name: 'notes',
+      component: Notes,
+    },
+    {
+      path: '/note/:id',
+      name: 'note',
+      component: Note,
+    },
+    {
+      path: '/editnote/:id',
+      name: 'editnote',
+      component: EditNote,
     },
     {
       path: '/about',
